4f64a0af431d925842d6dad3c87959540274bbe3
carlosg14 <169074726+carlosg14@users.noreply.github.com>
2025-06-10 13:31:33 -0300

    Add files via upload

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+// filepath: c:\Users\carlo\OneDrive\Escritorio\programas\Evidencia2\Evidencia2\postcss.config.cjs
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
